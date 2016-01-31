367c4b55e541f15aeaba79601a319a114d28a3d6
Neilson <hc3wong@uwaterloo.ca>
2016-01-31 14:42:44 -0500

    added Auth token generation when making a new user

--- User.js
+++ User.js
@@ -1,10 +1,13 @@
+var Auth = require('./auth');
+
 function User(firstName, lastName, nickName, email){
 	this.name = nickName || firstName;
 	this.firstName = firstName;
 	this.lastName = lastName;
 	this.fullName = firstName + ' ' + lastName;
-	this.email = email;
+	this.email = email;	//should be unique!
 	this.unsubscribe = false;
+	this.id = Auth.makeNewToken(email);
 }
 
 module.exports = User;
